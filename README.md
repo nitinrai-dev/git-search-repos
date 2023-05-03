c92584c2a0224d9d990f1314959f357a7bac71d2
Nitin Rai <nitinrai.dev@gmail.com>
2023-05-03 21:15:54 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# git-search-repos
- 
+# Github Search App
+
+## Follow steps to get started
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+## Task
+
+- Github provides APIs to access public repos. Link: https://developer.github.com/v3/search/
+- Using these APIs you have to complete the following tasks in React JS:
+Create a search field, which queries on public repos.
+Fetch the data of the repos and create a card for each.   
+- Following are the details, the card needs to have:
+Avatar, Repo name, Stars, Description, language 
+A Sort field with the following options-  Stars, watchers count, score, name,created_at, updated_at.
+
+Preview Link: [https://gitsearch-repos.vercel.app/](https://gitsearch-repos.vercel.app/)
---
+++ src/App.js
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import "./index.css";
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  const day = date.getDate().toString().padStart(2, "0");
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const year = date.getFullYear();
+
+  return `${day}/${month}/${year}`;
+}
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch(
+      `https://api.github.com/search/repositories?q=${searchTerm}`
+    );
+    const data = await response.json();
+    setSearchResults(data.items);
+  };
+
+  console.log(searchResults);
+
+  return (
+    <>
+      <form onSubmit={handleSearchSubmit}>
+        <div>
+          <label htmlFor="search-input">Search for GitHub repositories:</label>
+          <br />
+          <input
+            id="search-input"
+            placeholder="Search..."
+            type="search"
+            value={searchTerm}
+            onChange={handleSearchChange}
+          />
+          <button type="submit">Search</button>
+        </div>
+      </form>
+
+      {searchResults.length > 0 && (
+        <ul className="card-wrapper">
+          {searchResults.map((result) => (
+            <li key={result.id}>
+              <div className="card">
+                <div className="card-image">
+                  <img
+                    src={result.owner.avatar_url}
+                    alt={`Avatar of ${result.owner.login}`}
+                  />
+                  <small>{result.language}</small>
+                </div>
+                <h4>{result.full_name}</h4>
+                <small>{result.description}</small>
+                <div>
+                  Stars: {result.stargazers_count}
+                  <br />
+                  Watch: {result.watchers_count}
+                  <br />
+                  Score: {result.score}
+                  <br />
+                  Name: {result.name}
+                  <br />
+                </div>
+                <div>Created: {formatDate(result.created_at)}</div>
+                <div>Updated: {formatDate(result.updated_at)}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </>
+  );
+}
+
+export default App;
